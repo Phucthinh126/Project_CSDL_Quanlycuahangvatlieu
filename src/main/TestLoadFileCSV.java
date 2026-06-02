14717b8ebb736c86cc772122fcc4a07b7380a82f
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-06-03 01:40:46 +0700

    Test load file csv vao database

---
+++ src/main/TestLoadFileCSV.java
@@ -0,0 +1,24 @@
+package main;
+
+import dao.VatLieuDAO;
+
+public class TestLoadFileCSV {
+	public static void main(String[] args) {
+		VatLieuDAO vl = new VatLieuDAO();
+		// tai vat lieu vao database
+		String filePath = "D:\\10. SQL\\Project_QuanLyCuaHangVatTu\\QuanLyCuuHangVatTu\\src\\csv\\vatlieu.csv";
+		String filePath2 = "D:\\10. SQL\\Project_QuanLyCuaHangVatTu\\QuanLyCuuHangVatTu\\src\\csv\\vatlieumoi.csv";
+
+		// load csv vao database
+//		List<String> logs = CSVLoader.loadVatLieuFromCSV(filePath);
+//		System.out.println("\n=== KẾT QUẢ LOG CHẠY ĐƯỢC ===");
+//		for (String log : logs) {
+//			System.out.println(log);
+//		}
+
+		System.out.println("kiem tra vat lieu duoc them moi");
+		System.out.println(vl.getById("VL0081"));
+
+	}
+
+}
