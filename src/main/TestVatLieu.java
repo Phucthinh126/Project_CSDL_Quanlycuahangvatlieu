c0e748cafc69f2a0f62375d0dc74ab55dc0f07b5
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-06-05 23:46:18 +0700

    Test them vat lieu, them nha cung cap va chi tiet nha cung cap nhap hang cho vat lieu

--- src/main/TestVatLieu.java
+++ src/main/TestVatLieu.java
@@ -1,47 +1,61 @@
 package main;
 
-import dao.DonHangDAO;
-import dao.KhachHangDAO;
+import java.math.BigDecimal;
+import java.util.List;
+
+import dao.ChiTietNhaCungCapDAO;
+import dao.NhaCungCapDAO;
+import dao.VatLieuDAO;
+import dto.ChiTietNhaCungCapDTO;
+import dto.NhaCungCapDTO;
+import dto.VatLieuDTO;
 
 public class TestVatLieu {
 	public static void main(String[] args) {
-		KhachHangDAO khDAO = new KhachHangDAO();
-		DonHangDAO dhDAO = new DonHangDAO();
+		VatLieuDAO vlDao = new VatLieuDAO();
+		NhaCungCapDAO ncc = new NhaCungCapDAO();
+		ChiTietNhaCungCapDAO ctncc = new ChiTietNhaCungCapDAO();
 
-		// Lay tat ca danh sach khach hang
-//		List<KhachHangDTO> listAll = khDAO.getAll();
-//		for (KhachHangDTO e : listAll) {
+		// Lay toan bo vat lieu
+		List<VatLieuDTO> listVl = vlDao.getAll();
+		for (VatLieuDTO e : listVl) {
 //			System.out.println(e);
-//		}
-
-		// lay thong tin khach hang qua id
-//		System.out.println("thong tin khach hang");
-//		System.out.println(khDAO.getById("KH007"));
-
-		//
-
-//------------------------------Test chuc nang cua DonHang---------------------
-		// lay thong tin tat ca don hang
-//		List<DonHangDTO> listAllDH = dhDAO.getAll();
-//		for (DonHangDTO e : listAllDH) {
+		}
+		// Lay toan bo nha cung cap
+		List<NhaCungCapDTO> listNcc = ncc.getAll();
+		for (NhaCungCapDTO e : listNcc) {
 //			System.out.println(e);
-//		}
+		}
 
-//		// Danh sach chi tiet don hang thong qua ma don hang
-//		List<ChiTietDonHangDTO> getOrderDetails = dhDAO.getOrderDetails("DH150");
-//		for (ChiTietDonHangDTO e : getOrderDetails) {
+		// Lay toan bo chi tiet lan nhap hang
+		List<ChiTietNhaCungCapDTO> listCT = ctncc.getAll();
+		for (ChiTietNhaCungCapDTO e : listCT) {
 //			System.out.println(e);
-//		}
+		}
+
+		// tao vat lieu moi
+		VatLieuDTO vlMoi = new VatLieuDTO("VL0081", "Tấm panel cách nhiệt EPS (1.2m x 2.4m x 5cm)", "Tấm", 15,
+				new BigDecimal(320000));
+		// tao nha cung cap moi
+		NhaCungCapDTO nccMoi = new NhaCungCapDTO("NCC036", "Công ty TNHH Vật Liệu Xanh Việt", "02837123456",
+				"789 Đường số 8, KCN Tân Tạo, Quận Bình Tân, TP HCM");
+
+		// Tao chi tiet nha cung cap - so luong nhap hang vat lieu
+		ChiTietNhaCungCapDTO ctNccMoi = new ChiTietNhaCungCapDTO("NCC036", "VL0081", 1000, new BigDecimal(255000));
+		System.out.println("\nXem thong tin so luong mat hang trong vat lieu chua nhap hang cho vlMoi");
+		System.out.println(vlDao.getById("VL0081").getSoLuongTon());
+
 //
-//		// Doanh thu trong mot khoan thoi gian
-//		System.out
-//				.println("Doanh thu tu thang 4-5 nam 2026: " + dhDAO.getRevenueByDateRange("2026-04-01", "2026-04-01"));
-//		// Danh sach don hang cua khach hang
-//		System.out.println("Danh sach don hang cua khach hang");
-//		List<DonHangDTO> getOrdersByCustomer = dhDAO.getOrdersByCustomer("KH007");
-//		for (DonHangDTO e : getOrdersByCustomer) {
-//			System.out.println(e);
-//		}
+//		vlDao.insert(vlMoi); // them moi vat lieu
+//		ncc.insert(nccMoi); // them moi nha cc
+//		ctncc.insert(ctNccMoi); // cap nhat nhap hang cho vlMoi
+
+		System.out.println("Xem thong tin vat lieu moi: voi ma VL0081");
+		System.out.println(vlDao.getById("VL0081"));
+		System.out.println("\nXem thong tin nha cung cap moi voi ma: NCC036");
+		System.out.println(ncc.getById("NCC036"));
+		System.out.println("\nXem thong tin so luong mat hang trong vat lieu da nhap hang cho vlMoi");
+		System.out.println(vlDao.getById("VL0081").getSoLuongTon());
 
 	}
 
